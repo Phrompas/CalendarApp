57257922761e5c1f09ec79eb28570b006a0afa55
Phrompas <149044601+Phrompas@users.noreply.github.com>
2025-04-23 23:50:57 +0700

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -4,21 +4,17 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import eventRoutes from './routes/events';
 
-// โหลด .env
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/auth', authRoutes);
 app.use('/events', eventRoutes);
 
-// ทดสอบว่าเซิร์ฟเวอร์ยังอยู่
 app.get('/', (req, res) => {
   res.send('Calendar App Backend is running!');
 });
